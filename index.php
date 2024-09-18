66dcb3a28cf91e43d7955eeb743a257aa7c87687
Nelson Minero <ia.nelsonminero@ufg.edu.sv>
2024-09-17 20:55:28 -0600

    Commit inicial

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+require("conn.php");
+
+$arreglo = array(
+    "sucess"=>false,
+    "status"=>400,
+    "data"=>"",
+    "message"=>"",
+    "cant"=> 0
+);
+
+if($_SERVER["REQUEST_METHOD"] === "GET"){
+    //MÉTODO ES GET
+    if(isset($_GET["type"]) && ($_GET["type"] !="")){
+        //PARÁMETRO TYPE
+        $conexion = new conexion();
+        $conn = $conexion->conectar();
+        $datos = $conn->query("SELECT * FROM empleado;");
+        $resultados = $datos->fetchAll();
+
+        switch($_GET["type"]){
+            case "json":
+                result_json($resultados);
+                break;
+            case "xml":
+                result_xml($resultados);
+                break;
+            default;
+                echo("Por favor, defina el tipo de resultado");
+            break;
+        }
+
+
+    } else {
+        //NO HAY VALOR TYPE
+        $arreglo = array(
+            "sucess"=>false,
+            "status"=>array("status_code"=>412, "status_text"=>"Precondition Failed"),
+            "data"=>"",
+            "message"=>"SE ESPERABA PARÁMETRO TYPE",
+            "cant"=> 0
+        );
+
+    }
+} else {
+    //NO SE ACEPTA MÉTODO
+    $arreglo = array(
+        "sucess"=>false,
+        "status"=>array("status_code"=>405, "status_text"=>"Method Not Allowed"),
+        "data"=>"",
+        "message"=>"NO SE ACEPTA EL MÉTODO",
+        "cant"=> 0
+    );
+}
+
+
+function result_json($resultado){
+    $arreglo = array(
+        "sucess"=>true,
+        "status"=>array("status_code"=>200, "status_text"=>"OK"),
+        "data"=>$resultado,
+        "message"=>"",
+        "cant"=> sizeof($resultado)
+    );
+
+    header("HTTP/1.1 ".$arreglo["status"]["status_code"]." ".$arreglo["status"]["status_text"]);
+    header("Content-Type: Application/json");
+    echo(json_encode($arreglo));
+
+}
+
+function result_xml($resultado){
+    $xml = new SimpleXMLElement("<empleados />");
+    foreach($resultado as $i => $v){
+        $subnodo = $xml->addChild("empleado");
+        $invertir = array_flip($v);
+        array_walk_recursive($invertir,array($subnodo, "addChild"));
+    }
+    header("Content-Type: text/xml");
+    echo($xml->asXML());
+
+
+    // $arreglo = array(
+    //     "sucess"=>true,
+    //     "status"=>array("status_code"=>200, "status_text"=>"OK"),
+    //     "data"=>$resultado,
+    //     "message"=>"",
+    //     "cant"=> sizeof($resultado)
+    // );
+
+}
+
+?>
